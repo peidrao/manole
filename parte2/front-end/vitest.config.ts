83c6b4d042d012565f74ca8e84e6d5252cbe3286
Pedro Fonseca <contatopedrorn@gmail.com>
2026-03-19 19:39:36 -0300

    feat(config): add Vitest configuration files

---
+++ parte2/front-end/vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config';
+import { fileURLToPath, URL } from 'node:url';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./', import.meta.url)),
+    },
+  },
+  esbuild: {
+    jsx: 'automatic',
+    jsxImportSource: 'react',
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    globals: true,
+    include: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  },
+});
